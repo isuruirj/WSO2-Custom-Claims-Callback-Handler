4b73cb63f47ce100185c4cc372cf12f1f9a8bd78
isuru jayathilake <isuru@isuruj.local>
2020-01-30 14:46:54 +0530

    append application creator where the user is authenticated agains

--- src/main/java/org/wso2/carbon/identity/handler/IDTokenCustomClaims.java
+++ src/main/java/org/wso2/carbon/identity/handler/IDTokenCustomClaims.java
@@ -3,6 +3,10 @@ package org.wso2.carbon.identity.handler;
 import com.nimbusds.jwt.JWTClaimsSet;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.identity.oauth.IdentityOAuthAdminException;
+import org.wso2.carbon.identity.oauth.OAuthAdminService;
+import org.wso2.carbon.identity.oauth.dao.OAuthAppDO;
+import org.wso2.carbon.identity.oauth.dto.OAuthConsumerAppDTO;
 import org.wso2.carbon.identity.oauth2.authz.OAuthAuthzReqMessageContext;
 import org.wso2.carbon.identity.oauth2.token.OAuthTokenReqMessageContext;
 import org.wso2.carbon.identity.openidconnect.CustomClaimsCallbackHandler;
@@ -16,6 +20,24 @@ public class IDTokenCustomClaims extends DefaultOIDCClaimsCallbackHandler implem
     public JWTClaimsSet handleCustomClaims(JWTClaimsSet.Builder builder, OAuthTokenReqMessageContext request) {
 
         JWTClaimsSet jwtClaimsSet = super.handleCustomClaims(builder, request);
+        OAuthAdminService oAuthAdminService = new OAuthAdminService();
+        OAuthAppDO appDO = (OAuthAppDO) request.getProperty("OAuthAppDO");
+        String appName = null;
+        OAuthConsumerAppDTO consumerAppDTO = null;
+
+        if(appDO != null){
+            appName = appDO.getApplicationName();
+        }
+
+        try {
+            consumerAppDTO = oAuthAdminService.getOAuthApplicationDataByAppName(appName);
+        } catch (IdentityOAuthAdminException e) {
+            e.printStackTrace();
+        }
+
+        String creator = consumerAppDTO.getUsername();
+        builder.claim("spAppCreator",creator);
+
         if (jwtClaimsSet.getClaim("email") != null) {
             builder.claim("http://wso2.org/claims/emailaddress",jwtClaimsSet.getClaim("email"));
         }
@@ -31,6 +53,23 @@ public class IDTokenCustomClaims extends DefaultOIDCClaimsCallbackHandler implem
     public JWTClaimsSet handleCustomClaims(JWTClaimsSet.Builder builder, OAuthAuthzReqMessageContext request) {
 
         JWTClaimsSet jwtClaimsSet = super.handleCustomClaims(builder, request);
+        OAuthAdminService oAuthAdminService = new OAuthAdminService();
+        OAuthAppDO appDO = (OAuthAppDO) request.getProperty("OAuthAppDO");
+        String appName = null;
+        OAuthConsumerAppDTO consumerAppDTO = null;
+
+        if(appDO != null){
+            appName = appDO.getApplicationName();
+        }
+
+        try {
+            consumerAppDTO = oAuthAdminService.getOAuthApplicationDataByAppName(appName);
+        } catch (IdentityOAuthAdminException e) {
+            e.printStackTrace();
+        }
+
+        String creator = consumerAppDTO.getUsername();
+        builder.claim("spAppCreator",creator);
 
         if (jwtClaimsSet.getClaim("email") != null) {
             builder.claim("http://wso2.org/claims/emailaddress",jwtClaimsSet.getClaim("email"));
